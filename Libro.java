3d280327a7470eea36299dd1a8ec0c2edccc050e
Jorge Eduardo Montoya Torres <lalo_realmadrid93@hotmail.com>
2017-08-24 10:42:00 -0500

    Se hizo constructor

---
+++ Libro.java
@@ -0,0 +1,15 @@
+public class Libro
+{
+    // instance variables
+    private String t;
+    private String a;
+
+    /**
+     * Constructor
+     */
+    public Libro(String titulo, String autor)
+    {
+        t = titulo;
+        a = autor;
+    }
+}
