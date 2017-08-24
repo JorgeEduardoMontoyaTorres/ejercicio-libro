9d29990a7e0ad0b080917a483824ef5473486d95
Jorge Eduardo Montoya Torres <lalo_realmadrid93@hotmail.com>
2017-08-24 11:04:22 -0500

    Se creo un metodo de acceso dimeDetalles

--- Libro.java
+++ Libro.java
@@ -3,14 +3,16 @@ public class Libro
     // instance variables
     private String t;
     private String a;
+    private int pag;
 
     /**
      * Constructor
      */
-    public Libro(String titulo, String autor)
+    public Libro(String titulo, String autor, int paginas)
     {
         t = titulo;
         a = autor;
+        pag = paginas;
     }
     
     //Metodo de acceso
@@ -22,5 +24,28 @@ public class Libro
     public String dimeTitulo()
     {
         return t;
+    }
+    
+    public int dimePaginas()
+    {
+        return pag;
+    }
+    
+    public String dimeDetalles()
+    {
+        String cadResult = "";
+        cadResult += "Título";
+        cadResult += ": ";
+        cadResult += t;
+        cadResult += ", ";
+        cadResult += "Autor";
+        cadResult += ": ";
+        cadResult += a;
+        cadResult += ", ";
+        cadResult += "Páginas";
+        cadResult += ": ";
+        cadResult += pag;
+        
+        return cadResult;
     }
 }
